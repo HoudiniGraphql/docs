12e9a5aaee2fa1fef086af1ec5ee3ee441467420
Alec Aivazis <alec@aivazis.com>
2022-06-11 03:04:18 -0700

    selected store api is persisted across refreshes

---
+++ src/routes/api/_HeaderWithMode.svelte
@@ -0,0 +1,73 @@
+<script lang="ts">
+	import { session } from '$app/stores'
+
+	export let mode: 'inline' | 'store' = $session?.mode || 'inline'
+	export let title: string
+
+	function setMode(val: typeof mode) {
+		fetch('/setMode', {
+			method: 'POST',
+			body: JSON.stringify({
+				mode: val
+			})
+		})
+
+		$session.mode = val
+	}
+</script>
+
+<h1 id={title.toLowerCase()}>
+	{#if mode === 'store'}
+		{title} Store
+	{:else}
+		Inline {title}
+	{/if}
+
+	<fieldset>
+		<legend style="display: none;">Your favourite fruit?</legend>
+		<label>
+			<input
+				type="radio"
+				name="mode"
+				value="inline"
+				bind:group={mode}
+				on:click={() => setMode('inline')}
+			/>
+			<span class="mode-value">Inline</span>
+		</label>
+		<label>
+			<input
+				type="radio"
+				name="mode"
+				value="store"
+				bind:group={mode}
+				on:click={() => setMode('store')}
+			/>
+			<span class="mode-value">Store</span>
+		</label>
+	</fieldset>
+</h1>
+
+<style>
+	h1 {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+	}
+
+	input {
+		display: none;
+	}
+
+	.mode-value {
+		cursor: pointer;
+		font-size: 24px;
+		border-radius: 12px;
+		padding: 3px 20px;
+	}
+
+	input:checked ~ .mode-value {
+		background-color: #ff3e00;
+		color: #511400;
+	}
+</style>
---
+++ src/routes/setMode.ts
@@ -0,0 +1,13 @@
+export async function post({ locals, request, ...args }) {
+    const { mode } = await request.json()
+    if (!['store', 'inline'].includes(mode)) {
+        return {}
+    }
+
+    locals.session.data = { mode }
+
+    return {
+        body: "OK"
+    }
+
+}
