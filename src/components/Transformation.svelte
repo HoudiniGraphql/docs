c8f26c674bf486362caa2acd6c71734e9a45575c
Alec Aivazis <alec@aivazis.com>
2022-06-12 12:19:42 -0700

    dry transformation component

---
+++ src/components/Transformation.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { Icon } from '~/components'
+</script>
+
+<div style="display: flex; flex-direction: row; align-items: center; overflow-x: auto;">
+	<slot name="from" />
+	<p style="margin-left: 10px; margin-right: 10px">
+		<Icon name="arrow-right" height="30px" width="30px" />
+	</p>
+	<slot name="to" />
+</div>
