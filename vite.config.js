aa71e908b501e8ee5b6a97ff020fb81f7b2b474f
Chris Engelhard <c.engelhard@digi4care.nl>
2022-07-23 11:31:11 +0200

    prettyfied

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 // vite.config.js
-import { sveltekit } from '@sveltejs/kit/vite';
+import { sveltekit } from '@sveltejs/kit/vite'
 
 import path from 'path'
 
@@ -9,24 +9,24 @@ import { loadContent } from './src/lib/loadContent.js'
 
 /** @type {import('vite').UserConfig} */
 const config = {
-        optimizeDeps: {
-            include: ['highlight.js/lib/core']
-        },
-        resolve: {
-            alias: {
-                // these are the aliases and paths to them
-                '~': path.resolve('./src')
-            }
-        },
-        plugins: [
-            sveltekit(),
-            replaceCodePlugin({
-                replacements: [
-                    { from: 'REPLACE_WITH_OUTLINE', to: JSON.stringify(await loadOutline()) },
-                    { from: 'REPLACE_WITH_CONTENT', to: JSON.stringify(await loadContent()) }
-                ]
-            })
-        ]
-};
+	optimizeDeps: {
+		include: ['highlight.js/lib/core']
+	},
+	resolve: {
+		alias: {
+			// these are the aliases and paths to them
+			'~': path.resolve('./src')
+		}
+	},
+	plugins: [
+		sveltekit(),
+		replaceCodePlugin({
+			replacements: [
+				{ from: 'REPLACE_WITH_OUTLINE', to: JSON.stringify(await loadOutline()) },
+				{ from: 'REPLACE_WITH_CONTENT', to: JSON.stringify(await loadContent()) }
+			]
+		})
+	]
+}
 
-export default config;
+export default config
